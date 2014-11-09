33b012b8ecc699dcbc96f29770d80192b20ffbdb
sabakan21 <fujii@sein.mse.kyutech.ac.jp>
2014-11-10 02:58:38 +0900

    use dhcp arp table

--- dhcp_labwatch.rb
+++ dhcp_labwatch.rb
@@ -1,3 +1,4 @@
+=begin
 require "active_record"
 
 ActiveRecord::Base.configurations = YAML.load_file('database.yml')
@@ -5,6 +6,7 @@ ActiveRecord::Base.establish_connection('test')
 
 class User < ActiveRecord::Base
 end
+=end
 
 def ping(ip)
   r = Regexp.new('[1-9]\d* (packets )?received')
@@ -21,15 +23,14 @@ end
  
 def get_mac_addresses
   mac_adresses = Array.new
-  network_address = '172.20.0'
+  network_address = '192.168.63.'
  
-  2.upto(20) do |n| #nunber for ip
-    ip = '%s.%d' % [network_address, n]
-    if ping(ip)
-      mac_adresses << arp(ip)
-    end
-  end
- 
+  arp_table = `arp -n`
+  arp_table.each_line{|arp|
+    if arp.match(/0c:3e:9f:26:ee:8b/) {
+            ip = arp.match(/[0-9]+¥.[0-9]+¥.[0-9]+¥.[0-9]/)
+            puts ip
+  }
   mac_adresses
 end
  
